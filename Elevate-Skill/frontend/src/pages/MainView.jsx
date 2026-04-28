17813584c30d60f0adfe8bf717e51b30210c6486
Lidetu <lidosinafanis@gmail.com>
2026-04-28 21:23:25 +0300

    view update for courses page add

--- Elevate-Skill/frontend/src/pages/MainView.jsx
+++ Elevate-Skill/frontend/src/pages/MainView.jsx
@@ -3,12 +3,15 @@ import Services from "./Service";
 import Stories from "./Stories";
 import FAQ from "./FAQ";
 import Contact from "./Contact";
+import Courses from "./Courses";
+
 
 export default function MainView() {
   return (
     <>
       <section id="home"><Landing /></section>
       <section id="services"><Services /></section>
+      <section id="courses"><Courses /></section>
       <section id="stories"><Stories /></section>
       <section id="faq"><FAQ /></section>
       <section id="contact"><Contact /></section>
